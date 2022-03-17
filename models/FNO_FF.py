80a4a949dd320a57138e6689e530b3d13a7cc71e
Julian Parker <julian.parker@native-instruments.de>
2022-03-17 18:49:39 +0100

    1d feedforward network

---
+++ models/FNO_FF.py
@@ -0,0 +1,40 @@
+import torch
+from .layers import FourierConv2d
+
+class FNO_FF_1d(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, spatial_size, num_time_steps, width, depth = 4, activation = torch.nn.ReLU()):
+        super(FNO_FF_1d, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.width = width
+        self.depth = depth 
+        self.spatial_size = spatial_size
+        self.num_time_steps = num_time_steps
+
+        self.in_mapping = torch.nn.Linear(in_channels, self.width)
+
+        self.fourier_conv_layers = torch.nn.ModuleList()
+        for _ in range(self.depth):
+          self.fourier_conv_layers.append(FourierConv2d(self.width, self.width, self.spatial_size, self.num_time_steps))
+
+        self.w = torch.nn.ModuleList()
+        for _ in range(self.depth):
+          self.w.append(torch.nn.Conv2d(self.width, self.width, (1,1)))
+
+        self.activation = activation
+
+        self.out_mapping = torch.nn.Linear(self.width, out_channels)
+
+    def forward(self, x):
+        x = self.in_mapping(x)
+        o = torch.zeros(x.shape[0], self.num_time_steps,self.spatial_size,self.width).to(x.device)
+        o[:,0,:,:] = x.squeeze(1)
+        o = o.permute(0, 3, 1, 2)
+        for i in range(self.depth):
+          o1 = self.fourier_conv_layers[i](o)
+          o2 = self.w[i](o)
+          o = self.activation(o1) + o2
+        o = o.permute(0,2,3,1)
+        o = self.out_mapping(o)
+        return o
