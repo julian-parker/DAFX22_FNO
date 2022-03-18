12d4e1f5a7be06825fcd3ed3d2b7b5f8418c91f7
Julian Parker <julian.parker@native-instruments.de>
2022-03-18 17:42:05 +0100

    Add FourierConv3d

--- models/layers.py
+++ models/layers.py
@@ -20,7 +20,7 @@ class FourierConv1d(torch.nn.Module):
     def forward(self, x):
         batchsize = x.shape[0]
         if not self.periodic:
-          padding = x.shape[2]
+          padding = self.size
           x = torch.nn.functional.pad(x, [0,padding]) 
 
         x_ft = torch.fft.rfft(x)
@@ -66,3 +66,37 @@ class FourierConv2d(torch.nn.Module):
           x = x[..., :self.size_x, :self.size_y]
         return x
 
+class FourierConv3d(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, size_x, size_y, size_z, bias = True, periodic = False):
+        super(FourierConv2d, self).__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        if not periodic:
+          self.size_x = size_x
+          self.size_y = size_y
+          self.size_z = size_z
+        else:
+          self.size_x = size_x // 2
+          self.size_y = size_y // 2
+          self.size_z = size_z // 2
+
+        self.weights = torch.nn.Parameter((1 / (in_channels * out_channels)) * torch.complex(torch.rand(in_channels, out_channels, self.size_x, self.size_y, self.size_z),torch.rand(in_channels, out_channels, self.size_x, self.size_y, self.size_z)))
+        self.biases = torch.nn.Parameter((1 / out_channels) * torch.complex(torch.rand(out_channels, self.size_x, self.size_y, self.size_z),torch.rand(out_channels, self.size_x, self.size_y, self.size_z)))
+        self.bias = bias
+        self.periodic = periodic
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        if not self.periodic:
+          x = torch.nn.functional.pad(x, [0,self.size_z, 0, self.size_y, 0, self.size_x]) 
+
+        x_ft = torch.fft.rfft3(x)
+        out_ft = torch.zeros_like(x_ft)
+        out_ft[:, :, :self.size_x, :self.size_y] = torch.einsum("bixyz,ioxyz->boxyz",x_ft[:, :, :self.size_x, :self.size_y, :self.size_z], self.weights)
+        if self.bias:
+          out_ft[:, :, :self.size_x, :self.size_y, self.size_z] += self.biases
+        x = torch.fft.irfft3(out_ft)
+        if not self.periodic:
+          x = x[..., :self.size_x, :self.size_y, :self.size_z]
+        return x
