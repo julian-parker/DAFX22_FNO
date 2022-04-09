Fix normalization

train_1d_nonlinear_string.py
@@ -39,7 +39,7 @@ for i in range(num_variations):
     y_x, y_defl_x = stringSolver.solve(fe_x)
     training_input[i,:,:,:] = torch.tensor(np.stack([y_x[:,0], y_defl_x[:,0]], axis = -1 )).unsqueeze(0)
     training_output[i,:,:,:] = torch.tensor(np.stack([y_x[:,1:].transpose(), y_defl_x[:,1:].transpose()], axis = -1 )).unsqueeze(0)
-normalization_multiplier = 1/torch.std(training_input)
+normalization_multiplier = 1/torch.std(training_output, dim = (0,1,2))
 training_input *= normalization_multiplier
 training_output *= normalization_multiplier
 
@@ -127,8 +127,9 @@ stringSolver = TensionModulatedStringSolver(dur = dur, Fs = fs,delta_x = delta_x
 fe_x = stringSolver.create_pluck(0.49, max_pluck_deflection)
 y_x, y_defl_x = stringSolver.solve(fe_x)
 model_input = torch.tensor(np.stack([y_x[:,0], y_defl_x[:,0]], axis = -1 )).unsqueeze(0).to(device)
-model_input *= 1 / model_input.std(dim=(0,1,2))
-y_x *= 1 / model_input.std().cpu().numpy()
+model_input *= normalization_multiplier
+y_x *= normalization_multiplier[0]
+
 output_sequence_gru = model_gru(model_input, num_example_timesteps)
 output_sequence_rnn = model_rnn(model_input, num_example_timesteps)
 output_sequence_ref = model_ref(model_input, num_example_timesteps)

train_1d_string.py
@@ -37,7 +37,7 @@ for i in range(num_variations):
     y_x, y_defl_x = stringSolver.solve(fe_x)
     training_input[i,:,:,:] = torch.tensor(np.stack([y_x[:,0], y_defl_x[:,0]], axis = -1 )).unsqueeze(0)
     training_output[i,:,:,:] = torch.tensor(np.stack([y_x[:,1:].transpose(), y_defl_x[:,1:].transpose()], axis = -1 )).unsqueeze(0)
-normalization_multiplier = 1/torch.std(training_input, dim=(0,1,2))
+normalization_multiplier = 1/torch.std(training_output, dim=(0,1,2))
 training_input *= normalization_multiplier
 training_output *= normalization_multiplier
 
@@ -125,8 +125,8 @@ stringSolver = StringSolver(dur = dur, Fs = fs,delta_x = delta_x, d1 = d1)
 fe_x = stringSolver.create_pluck(0.49)
 y_x, y_defl_x = stringSolver.solve(fe_x)
 model_input = torch.tensor(np.stack([y_x[:,0], y_defl_x[:,0]], axis = -1 )).unsqueeze(0).to(device)
-model_input *= 1 / model_input.std(dim=(0,1,2))
-y_x *= 1 / model_input.std().cpu().numpy()
+model_input *= normalization_multiplier
+y_x *= normalization_multiplier[0]
 output_sequence_gru = model_gru(model_input, num_example_timesteps)
 output_sequence_rnn = model_rnn(model_input, num_example_timesteps)
 output_sequence_ref = model_ref(model_input, num_example_timesteps)
@@ -141,7 +141,6 @@ plt.rcParams.update({
     "font.size": 10,
     "font.serif": ["Times"]})
 gs = fig.add_gridspec(1, 4, hspace=0, wspace=0.05)
-
 
 axs = gs.subplots(sharex='row', sharey=True)
 axs[0].imshow(output_sequence_gru[0,:,:,0].detach().cpu().numpy(),cmap = 'Greys', aspect = 'auto')

train_2d_wave.py
@@ -38,7 +38,7 @@ for i in range(num_variations):
     training_input[i,:,:,:,:] = torch.tensor(np.stack([y_sp[:,:,0], y_vx[:,:,0], y_vy[:,:,0]], axis = -1 )).unsqueeze(0)
     training_output[i,:,:,:,:] = torch.tensor(np.stack([y_sp[:,:,1:].transpose(2,0,1), y_vx[:,:,1:].transpose(2,0,1), y_vy[:,:,1:].transpose(2,0,1)], axis = -1 )).unsqueeze(0)
     
-normalization_multiplier = 1/torch.std(training_input, dim = (0,1,2,3))
+normalization_multiplier = 1/torch.std(training_output, dim = (0,1,2,3))
 training_input *= normalization_multiplier
 training_output *= normalization_multiplier
 
@@ -130,8 +130,8 @@ solver = WaveSolver2D(dur = dur, Fs = fs, lx = room_size, ly = room_size, spatia
 fe_x = solver.create_impulse(0.5,0.1)
 _,_,y_x, y_vx, y_vy = solver.solve(fe_x)
 model_input = torch.tensor(np.stack([y_x[:,:,0], y_vx[:,:,0], y_vy[:,:,0]], axis = -1 )).unsqueeze(0).to(device)
-model_input *= 1 / model_input.std(dim = (0,1,2,3))
-y_x *= 1 / model_input.std().cpu().numpy()
+model_input *= normalization_multiplier
+y_x *= normalization_multiplier[0]
 output_sequence_gru = model_gru(model_input, num_example_timesteps)
 output_sequence_rnn = model_rnn(model_input, num_example_timesteps)
 output_sequence_ref = model_ref(model_input, num_example_timesteps)