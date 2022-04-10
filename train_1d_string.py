122a5bb876d123995860741525d27aa056efd886
Julian Parker <julian.parker@cantab.net>
2022-04-10 10:54:15 +0200

    fix device during plotting

--- train_1d_string.py
+++ train_1d_string.py
@@ -125,7 +125,7 @@ stringSolver = StringSolver(dur = dur, Fs = fs,delta_x = delta_x, d1 = d1)
 fe_x = stringSolver.create_pluck(0.49)
 y_x, y_defl_x = stringSolver.solve(fe_x)
 model_input = torch.tensor(np.stack([y_x[:,0], y_defl_x[:,0]], axis = -1 )).unsqueeze(0).to(device)
-model_input *= normalization_multiplier
+model_input *= normalization_multiplier.to(device)
 y_x *= normalization_multiplier[0]
 output_sequence_gru = model_gru(model_input, num_example_timesteps)
 output_sequence_rnn = model_rnn(model_input, num_example_timesteps)
